82786c2686179af17dc76d2c2f3c4097b9317313
Matthew Honnibal <honnibal+gh@gmail.com>
2022-12-19 17:48:50 +0100

    Add test for duplicate spans in stream. Also add helpers for tests (#34)
    * Add test for duplicate spans in stream. Also add helpers for tests
    
    * Update openai-ner/tests/test_ner.py
    
    Co-authored-by: Sofie Van Landeghem <svlandeg@users.noreply.github.com>

--- openai-ner/tests/test_ner.py
+++ openai-ner/tests/test_ner.py
@@ -1,5 +1,13 @@
+from collections import defaultdict
 from pathlib import Path
-from recipes.ner import _find_substrings, _load_template, PromptExample
+from typing import Dict, List, Tuple
+
+import pytest
+import spacy
+
+from recipes.openai_ner import PromptExample, _find_substrings, _load_template
+
+from .utils import make_suggester
 
 
 def test_multiple_substrings():
@@ -95,3 +103,84 @@ PLACE: Germany
 
 """.lstrip()
     )
+
+
+@pytest.mark.parametrize(
+    "comment,text,raw_spans,filtered_spans",
+    [
+        (
+            "Does it take longest when nested and shorter is first?",
+            "one two three four",
+            [("a", 0, 1), ("b", 0, 2)],
+            [("b", 0, 2)],
+        ),
+        (
+            "Does it take longest when nested and shorter is last?",
+            "one two three four",
+            [("b", 0, 2), ("a", 0, 1)],
+            [("b", 0, 2)],
+        ),
+        (
+            "Does it take first when overlapping and shorter is first?",
+            "one two three four",
+            [("b", 0, 2), ("a", 1, 3)],
+            [("b", 0, 2)],
+        ),
+        (
+            "Does it take first when overlapping and shorter is last?",
+            "one two three four",
+            [("a", 0, 2), ("b", 1, 3)],
+            [("a", 0, 2)],
+        ),
+    ],
+)
+def test_one_token_per_span(
+    comment: str,
+    text: str,
+    raw_spans: List[Tuple[str, int, int]],
+    filtered_spans: List[Tuple[int, int]],
+):
+    labels = list(sorted(set(label for label, _, _1 in raw_spans)))
+    suggester = make_suggester(
+        labels=labels, openai_api_key="fake api key", openai_api_org="fake api org"
+    )
+    prompt = suggester._get_ner_prompt(text, labels=labels, examples=[])
+    response = _get_response(text, labels, raw_spans)
+    stream = [{"text": text, "openai": {"prompt": prompt, "response": response}}]
+    stream = list(suggester.format_suggestions(stream, nlp=spacy.blank("en")))
+    output_spans = [
+        (s["label"], s["token_start"], s["token_end"] + 1) for s in stream[0]["spans"]
+    ]
+    assert output_spans == filtered_spans
+    # Also check there's no overlaps
+    seen_tokens = set()
+    for _, start, end in output_spans:
+        for i in range(start, end):
+            assert i not in seen_tokens, "Overlapping or nested spans found"
+            seen_tokens.add(i)
+
+
+def _get_response(text: str, labels, spans: List[Tuple[str, int, int]]) -> str:
+    # Get table of start and end character offsets for the test spans.
+    tokens = text.split()
+    start_chars, end_chars = _get_token_char_maps(tokens, [True for _ in tokens])
+    spans_by_label = defaultdict(list)
+    for label, start, end in spans:
+        spans_by_label[label].append(text[start_chars[start] : end_chars[end - 1]])
+    response_lines = []
+    for label in labels:
+        response_lines.append(f"{label}: {', '.join(spans_by_label[label])}")
+    return "\n".join(response_lines)
+
+
+def _get_token_char_maps(
+    tokens: List[str], whitespace: List[bool]
+) -> Tuple[Dict[int, int], Dict[int, int]]:
+    idx = 0
+    start_chars = {}
+    end_chars = {}
+    for i, token in enumerate(tokens):
+        start_chars[i] = idx
+        idx += len(token) + int(whitespace[i])
+        end_chars[i] = idx
+    return start_chars, end_chars
