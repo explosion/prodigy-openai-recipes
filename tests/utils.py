6d36d663856279311fbdcb311219c664d89be3f7
Lj Miranda <ljvmiranda@gmail.com>
2023-01-04 10:18:56 +0800

    Refactor NER based on new OpenAISuggester API

--- tests/utils.py
+++ tests/utils.py
@@ -1,12 +1,18 @@
-from typing import List
+from typing import List, Callable
+from pathlib import Path
 
-from recipes.openai_ner import (DEFAULT_PROMPT_PATH, OpenAISuggester,
-                                _get_api_credentials, _load_template)
+from recipes.openai import get_api_credentials, load_template, OpenAISuggester
 
 
-def make_suggester(labels: List[str], **kwargs) -> OpenAISuggester:
+def make_suggester(
+    response_parser: Callable,
+    labels: List[str],
+    prompt_path: Path,
+    model: str = "text-davinci-003",
+    **kwargs
+) -> OpenAISuggester:
     if "openai_api_key" not in kwargs or "openai_api_org" not in kwargs:
-        api_key, api_org = _get_api_credentials(model)
+        api_key, api_org = get_api_credentials(model)
         if "openai_api_key" not in kwargs:
             kwargs["openai_api_key"] = api_key
         if "openai_api_org" not in kwargs:
@@ -14,9 +20,9 @@ def make_suggester(labels: List[str], **kwargs) -> OpenAISuggester:
     if "max_examples" not in kwargs:
         kwargs["max_examples"] = 0
     if "prompt_template" not in kwargs:
-        kwargs["prompt_template"] = _load_template(DEFAULT_PROMPT_PATH)
+        kwargs["prompt_template"] = load_template(prompt_path)
     if "segment" not in kwargs:
         kwargs["segment"] = False
     if "openai_model" not in kwargs:
         kwargs["openai_model"] = "text-davinci-003"
-    return OpenAISuggester(labels=labels, **kwargs)
+    return OpenAISuggester(response_parser=response_parser, labels=labels, **kwargs)
