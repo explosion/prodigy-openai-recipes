315595b47eac732ea6ef0c0c11c7a03ef7164da9
Lj Miranda <ljvmiranda@gmail.com>
2023-01-24 09:52:55 +0800

    Remove make_suggester refactor
    Add it in during the actual PR. No need to duplicate the code here.

--- tests/test_textcat.py
+++ tests/test_textcat.py
@@ -8,30 +8,6 @@ from recipes.openai import OpenAISuggester, get_api_credentials, load_template
 from recipes.openai_textcat import DEFAULT_PROMPT_PATH, make_textcat_response_parser
 
 
-def make_suggester(
-    response_parser: Callable,
-    labels: List[str],
-    prompt_path: Path,
-    model: str = "text-davinci-003",
-    **kwargs
-) -> OpenAISuggester:
-    if "openai_api_key" not in kwargs or "openai_api_org" not in kwargs:
-        api_key, api_org = get_api_credentials(model)
-        if "openai_api_key" not in kwargs:
-            kwargs["openai_api_key"] = api_key
-        if "openai_api_org" not in kwargs:
-            kwargs["openai_api_org"] = api_org
-    if "max_examples" not in kwargs:
-        kwargs["max_examples"] = 0
-    if "prompt_template" not in kwargs:
-        kwargs["prompt_template"] = load_template(prompt_path)
-    if "segment" not in kwargs:
-        kwargs["segment"] = False
-    if "openai_model" not in kwargs:
-        kwargs["openai_model"] = "text-davinci-003"
-    return OpenAISuggester(response_parser=response_parser, labels=labels, **kwargs)
-
-
 @pytest.mark.parametrize(
     # fmt: off
     "response,answer",
